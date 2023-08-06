099e8d079533e100fa671150fec672b949b5c1bb
Glitch-art <56557289+Glitch-art@users.noreply.github.com>
2023-08-06 00:59:05 -0500

    fix build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -67,17 +67,17 @@ dependencies {
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
 
-    implementation "androidx.wear:wear:1.2.0"
+    implementation("androidx.wear:wear:1.2.0")
 
     // Add support for wearable specific inputs
-    implementation "androidx.wear:wear-input:1.1.0"
-    implementation "androidx.wear:wear-input-testing:1.1.0"
+    implementation("androidx.wear:wear-input:1.1.0")
+    implementation("androidx.wear:wear-input-testing:1.1.0")
 
     // Use to implement wear ongoing activities
-    implementation "androidx.wear:wear-ongoing:1.0.0"
+    implementation("androidx.wear:wear-ongoing:1.0.0")
 
     // Use to implement support for interactions from the Wearables to Phones
-    implementation "androidx.wear:wear-phone-interactions:1.0.1"
+    implementation("androidx.wear:wear-phone-interactions:1.0.1")
     // Use to implement support for interactions between the Wearables and Phones
-    implementation "androidx.wear:wear-remote-interactions:1.0.0"
+    implementation("androidx.wear:wear-remote-interactions:1.0.0")
 }
